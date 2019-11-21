ceed81bb66dbd89e1b98968c97deea71ee424fbc
tom <tom.block@brockmann-consult.de>
2019-11-21 17:16:37 +0100

    initial tests

--- app/renderer.js
+++ app/renderer.js
@@ -1,1 +1,7 @@
+// console.log("Gnaaaaaarz!!!", require);
+
+// var moduleName = './renderer/main.js';
+// require([moduleName], function(main){
+//     main()
+// })
 require('./renderer/main.js').main();
